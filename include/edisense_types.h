9bae7a59c6f176c72f782634d6ba8a854c7113d5
James Hong <jamesh93@stanford.edu>
2014-12-02 00:41:53 -0800

    updated edisense types

--- include/edisense_types.h
+++ include/edisense_types.h
@@ -17,9 +17,52 @@ typedef uint16_t partition_t;
 // 64-bit transaction id
 typedef uint64_t transaction_t;
 
-typedef struct {
-  node_t node;
-  std::list<int> range; // TODO I don't know how we want to represent ranges?
-} Ownership;
+const int kMaxDataLen = 20;
+
+typedef struct Data
+{
+	time_t timestamp;
+	time_t expiration;
+	char data[kMaxDataLen];
+	size_t datalen;
+} Data;
+
+// for message passing
+
+enum ErrorType
+{
+	DATA_MOVED = 0,
+	DATA_NOT_OWNED = 1,
+	DATA_MOVING = 2,
+	DB_ERROR = 3
+};
+
+typedef struct MessageId
+{
+	node_t node_id;
+	transaction_t tid;
+} MessageId;
+
+typedef struct PutResult
+{
+	bool success;
+	ErrorType error;
+	node_t moved_to;
+} PutResult;
+
+typedef struct CanReceiveResult 
+{
+	bool can_recv;
+	float util;
+	size_t free;
+} CanReceiveResult;
+
+typedef struct GetPartitionTableResult
+{
+	bool success;
+	int num_partitions;
+	int num_replicas;
+	node_t *partition_table; // DO NOT WRITE TO OR FREE THIS!!!
+} GetPartitionTableResult;
 
 #endif /* EDISENSE_TYPES_H */
